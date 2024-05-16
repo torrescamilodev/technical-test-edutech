c3d75cffb6024213e5a02cd8b0bfa7c8c8081f91
Camilo Andres Torres <torrescamilodev@gmail.com>
2024-05-16 15:19:04 -0500

    added instructions README.md and fixed components/Chat.js

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -12,9 +12,18 @@ const Chat = () => {
     });
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api/chat/messages', {
-            headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
-        }).then(res => setMessages(res.data));
+        const fetchMessages = async () => {
+            try {
+                const res = await axios.get('http://localhost:5000/api/chat/messages', {
+                    headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+                });
+                setMessages(res.data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        fetchMessages();
 
         socket.on('message', (msg) => {
             setMessages((prevMessages) => [...prevMessages, msg]);
@@ -26,26 +35,30 @@ const Chat = () => {
     }, [socket]);
 
     const sendMessage = async () => {
-        const res = await axios.post('http://localhost:5000/api/chat/message', { text }, {
-            headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
-        });
-        socket.emit('chatMessage', res.data);
-        setText('');
+        try {
+            const res = await axios.post('http://localhost:5000/api/chat/message', { text }, {
+                headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+            });
+            socket.emit('chatMessage', res.data);
+            setText('');
+        } catch (err) {
+            console.error(err);
+        }
     };
 
     return (
         <div>
             <div>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/313OAdZakyY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/S5FlIUvYBqA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
             <div>
-                {messages.map(msg => (
+                {messages.map((msg) => (
                     <div key={msg._id}>
                         <strong>{msg.user.name}</strong>: {msg.text}
                     </div>
                 ))}
             </div>
-            <input value={text} onChange={e => setText(e.target.value)} />
+            <input value={text} onChange={(e) => setText(e.target.value)} />
             <button onClick={sendMessage}>Send</button>
         </div>
     );
